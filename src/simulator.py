00f5323246d94a6de7a3d5f3110bdcb9de94a345
Francesco Lotito <lotitoqf@gmail.com>
2020-05-02 16:58:33 +0200

    add an irradiation model

--- src/simulator.py
+++ src/simulator.py
@@ -1,6 +1,23 @@
-from .network import Network
+from network import Network, Node
+import math
 
 
 class Simulator:
     def __init__(self, N_common, N_influencers, N_interests, random_const, random_phy_const):
         self.network = Network(N_common, N_influencers, N_interests, random_const, random_phy_const)
+
+def irradiate(a : Node, b : Node):
+    En = 1
+    #permeability
+    a.education_rate = 0.8
+    #proximity
+    d = 0.9
+    mb = 1
+    alpha = 1
+    beta = 2
+    return En * a.education_rate * ((alpha * d + beta * mb) / (alpha + beta))
+
+if __name__ == "__main__":
+    a = Node(1, 1, 5)
+    b = Node(1, 1, 5)
+    print(irradiate(a, b))
