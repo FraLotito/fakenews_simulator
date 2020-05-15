303d9aa8318e7a2d175243aa4648f5c56097b5a0
Francesco Lotito <lotitoqf@gmail.com>
2020-05-15 16:46:31 +0200

    add exp scripts

---
+++ experiments_scripts/network.py
@@ -0,0 +1,300 @@
+from enum import IntEnum
+import random
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def euclidean_distance(a, b):
+    return np.linalg.norm(a - b)
+
+
+def norm_sample(avg=0.0, var=0.4, clip1=-1, clip2=1, n=None):
+    norm_vals = np.random.normal(avg, var, n)  # sample from a normal distribution
+    return np.clip(norm_vals, clip1, clip2)  # limit the results into [0, 1]
+
+
+class NodeType(IntEnum):
+    Common = 0,
+    Conspirator = 1,
+    Influencer = 2,
+    Debunker = 3,
+    Bot = 4
+
+
+class Edge:
+    def __init__(self, start, dest, weight):
+        self.start = start
+        self.dest = dest
+        self.weight = weight
+
+
+class Node:
+    def __init__(self, _id, node_type: NodeType, n_interests, int_avg, int_var, recover_avg, recover_var,
+                 vuln_avg, vuln_var, reshare_avg, reshare_var):
+        self.id = _id
+        self.type = node_type
+        self.adj = []
+
+        self.score = 0
+        self.recover_rate = norm_sample(avg=recover_avg, var=recover_var, clip1=0, clip2=1)
+        self.vulnerability = norm_sample(avg=vuln_avg, var=vuln_var, clip1=-1, clip2=1)
+        self.reshare_rate = norm_sample(avg=reshare_avg, var=reshare_var, clip1=0, clip2=1)
+
+        self.interests = norm_sample(avg=int_avg, var=int_var, n=n_interests, clip1=-1, clip2=1)
+
+        np.append(self.interests, self.vulnerability)
+        np.append(self.interests, self.recover_rate)
+        np.append(self.interests, self.score)
+
+        self.position_x = np.random.uniform(0, 1)
+        self.position_y = np.random.uniform(0, 1)
+        self.message_queue = []
+
+    def is_recovered(self):
+        p = random.uniform(0, 1)
+        if p < self.recover_rate:
+            self.score = -1
+            return True
+        else:
+            return False
+
+    def update(self):
+        s = sum(self.message_queue)
+        l = len(self.message_queue)
+        self.message_queue = []
+        if l > 0:
+            avg = s / l
+            self.score += avg
+            if self.score > 1:
+                self.score = 1
+            elif self.score < - 1:
+                self.score = -1
+            return True
+        else:
+            return False
+
+    def update_sir(self):
+        number_of_messages = len(self.message_queue)
+
+        if self.type == NodeType.Bot:
+            return
+
+        if number_of_messages != 0 and self.score != -1:
+            can_fact_check = False
+
+            for i in range(number_of_messages):
+                p = random.uniform(0, 1)
+                message_type = self.message_queue[i]
+
+                if message_type == -1:
+                    k = 0.1
+                else:
+                    k = 1
+                    can_fact_check = True
+
+                if p < self.vulnerability * k:
+                    self.score = message_type
+                    can_fact_check = False
+
+            if can_fact_check and self.score == 0:
+                self.is_recovered()
+
+        self.message_queue = []
+
+    def add_adj(self, edge):
+        self.adj.append(edge)
+
+    def compute_distance(self, node_b):
+        return euclidean_distance(self.interests, node_b.interests) / math.sqrt(len(self.interests))
+
+    def compute_physical_distance(self, node_b):
+        a = np.array([self.position_x, self.position_y])
+        b = np.array([node_b.position_x, node_b.position_y])
+        return euclidean_distance(a, b) / math.sqrt(2)
+
+    def __str__(self):
+        return '{} - deg: {}\n' \
+               '\tinterests: {}\n' \
+               '\tscore: {}\n' \
+               '\tx: {} y: {}'.format(
+                self.id, len(self.adj), self.interests, self.score,
+                self.position_x, self.position_y)
+
+
+class Network:
+    def __init__(self, N_common, N_influencers, N_bots, N_interests, random_const, random_phy_const,
+                 int_avg, int_var, recover_avg, recover_var, vuln_avg, vuln_var, reshare_avg, reshare_var,
+                 weighted=True):
+        self.N_common = N_common
+        self.N_influencers = N_influencers
+        self.N_bots = N_bots
+
+        self.is_weighted = weighted
+        self.nodes = {}
+        self.available_id = 0
+        self.N_interests = N_interests
+        self.recover_avg, self.recover_var = recover_avg, recover_var
+        self.vuln_avg, self.vuln_var = vuln_avg, vuln_var
+        self.reshare_avg, self.reshare_var = reshare_avg, reshare_var
+        self.int_avg, self.int_var = int_avg, int_var
+
+        # First node that starts infection. Useful for statistics
+        self.infected_node = None
+
+    def gen_node(self, node_type):
+        idx = self.available_id
+        node = Node(idx, node_type, n_interests=self.N_interests, int_avg=self.int_avg, int_var=self.int_var,
+                    recover_avg=self.recover_avg, recover_var=self.recover_var, vuln_avg=self.vuln_avg, vuln_var=self.vuln_var,
+                    reshare_avg=self.reshare_avg, reshare_var=self.reshare_var)
+        self.available_id += 1
+        self.nodes[node.id] = node
+        return idx
+
+    def generate_common(self, random_const, random_phy_const):
+
+        def add_proximity_edge(idx_a, idx_b, dist, random_const):
+            prox = (1 - dist)
+            edge = list(filter(lambda x: x.dest == idx_b, self.nodes[idx_a].adj))
+            p = random.uniform(0, 1)
+            if (dist < random_const or len(edge) > 0) and p < 0.5:
+                if self.is_weighted:
+                    weight = prox
+                else:
+                    weight = 1
+                if len(edge) == 0:
+                    self.nodes[idx_a].add_adj(Edge(idx_a, idx_b, weight))
+                    self.nodes[idx_b].add_adj(Edge(idx_b, idx_a, weight))
+                else:
+                    weight = np.min([weight, edge[0].weight])
+                    edge_b = list(filter(lambda x: x.dest == idx_a, self.nodes[idx_b].adj))
+                    edge[0].weight = weight
+                    edge_b[0].weight = weight
+
+        n = 0
+        while n < self.N_common:
+            idx = self.gen_node(NodeType.Common)
+
+            for b in self.nodes.keys():
+                if idx == b:
+                    continue
+                phys_dist = self.nodes[idx].compute_physical_distance(self.nodes[b])
+                add_proximity_edge(idx, b, phys_dist, random_phy_const)
+            n += 1
+
+        for a in self.nodes.keys():
+            for b in self.nodes.keys():
+                if a == b:
+                    continue
+                dist = self.nodes[a].compute_distance(self.nodes[b])
+                add_proximity_edge(a, b, dist, random_const)
+
+    # TODO: fare attenzione alle random_const (ci sono costanti moltiplicative)
+
+    def generate_influencers(self, random_const, random_phy_const, lim=None):
+        print("---- INFLUECERS INFO: -----")
+
+        def add_proximity_edge(idx_a, idx_b, dist, random_const):
+            prox = (1 - dist)
+            if dist < random_const:
+                edge = list(filter(lambda x: x.dest == idx_b, self.nodes[idx_a].adj))
+                if self.is_weighted:
+                    weight = prox
+                else:
+                    weight = 1
+                if len(edge) == 0:
+                    self.nodes[idx_a].add_adj(Edge(idx_a, idx_b, weight))
+                else:
+                    weight = np.mean([weight, edge[0].weight])
+                    edge[0].weight = weight
+
+        n = 0
+        if lim == None:
+            lim = self.N_influencers
+
+        while n < lim:
+            idx = self.gen_node(NodeType.Influencer)
+
+            for b in self.nodes.keys():
+                if idx == b:
+                    continue
+                dist = self.nodes[idx].compute_distance(self.nodes[b])
+                add_proximity_edge(idx, b, dist, random_const * 2)
+
+                phys_dist = self.nodes[idx].compute_physical_distance(self.nodes[b])
+                add_proximity_edge(idx, b, phys_dist, random_phy_const * 2)
+
+            for b in self.nodes.keys():
+                if idx == b:
+                    continue
+                dist = self.nodes[idx].compute_distance(self.nodes[b])
+                add_proximity_edge(b, idx, dist, random_const)
+
+                phys_dist = self.nodes[idx].compute_physical_distance(self.nodes[b])
+                add_proximity_edge(b, idx, phys_dist * 0.5, random_phy_const * 0.5)
+
+            print("OUT: {}".format(len(self.nodes[idx].adj)))
+            cont = 0
+            for i in self.nodes.keys():
+                for j in self.nodes[i].adj:
+                    if j.dest == idx:
+                        cont += 1
+
+            print("IN: {}".format(cont))
+
+
+            n += 1
+
+    def generate_bots(self, lim=None):
+        print("---- BOTS INFO: -----")
+        n = 0
+        if lim == None:
+            lim = self.N_bots
+
+        while n < lim:
+            idx = self.gen_node(NodeType.Bot)
+            self.nodes[idx].score = 1
+
+            for b in self.nodes.keys():
+                if self.nodes[b].type == NodeType.Bot:
+                    continue
+
+                p = random.uniform(0, 1)
+                if p < 0.05:
+                    if self.is_weighted:
+                        weight = 0.1
+                    else:
+                        weight = 1
+                    self.nodes[idx].add_adj(Edge(idx, b, weight))
+                    self.nodes[idx].add_adj(Edge(b, idx, weight))
+
+            n+=1
+
+            print("OUT: {}".format(len(self.nodes[idx].adj)))
+            cont = 0
+            for i in self.nodes.keys():
+                for j in self.nodes[i].adj:
+                    if j.dest == idx:
+                        cont += 1
+
+            print("IN: {}".format(cont))
+
+
+    def average_score(self):
+        tot = 0
+        for n in self.nodes:
+            tot += self.nodes[n].score
+        return tot / len(self.nodes)
+
+    def average_weight(self):
+        tot = 0
+        for n in self.nodes:
+            weights = list(map(lambda edge: edge.weight, self.nodes[n].adj))
+            if len(weights) == 0:
+                tot += 0
+            else:
+                tot += sum(weights) / len(weights)
+        return tot / len(self.nodes)
+
+    def count_score_equal(self, value):
+        return len(list(filter(lambda n: self.nodes[n].score == value, self.nodes)))
---
+++ experiments_scripts/sir_report.py
@@ -0,0 +1,326 @@
+from network import Network, Node, NodeType
+import math
+from queue import PriorityQueue
+from random import expovariate, shuffle, uniform, randint
+import copy
+import pickle
+import matplotlib.pyplot as plt
+import numpy as np
+import networkx as nx
+
+class Simulator:
+    def __init__(self, N_common, N_influencers, N_bots, N_interests, random_const, random_phy_const, engagement_news,
+                 int_avg, int_var, recover_avg, recover_var, vuln_avg, vuln_var, reshare_avg, reshare_var, weighted=True):
+
+        self.N_common = N_common
+        self.N_influencers = N_influencers
+        self.N_bots = N_bots
+        self.random_const = random_const
+        self.random_phy_const = random_phy_const
+        self.engagement_news = engagement_news
+        self.network = Network(N_common=N_common, N_influencers=N_influencers, N_bots=N_bots, N_interests=N_interests,
+                               random_const=random_const, random_phy_const=random_phy_const, int_avg=int_avg, int_var=int_var,
+                               recover_avg=recover_avg, recover_var=recover_var, vuln_avg=vuln_avg, vuln_var=vuln_var,
+                               reshare_avg=reshare_avg, reshare_var=reshare_var, weighted=weighted)
+
+        self.network.generate_common(self.random_const, self.random_phy_const)
+        self.N = len(self.network.nodes)
+        self.sim_network = None
+
+    def add_influencers(self, n):
+        self.network.generate_influencers(self.random_const, self.random_phy_const, n)
+        self.N = len(self.network.nodes)
+
+    def add_bots(self, n):
+        self.network.generate_bots(n)
+        self.N = len(self.network.nodes)
+
+    def first_population_queue(self, first_infect):
+        order = []
+        self.events_queue.put((0, first_infect))
+        for i in range(self.N):
+            if i != first_infect:
+                order.append(i)
+        shuffle(order)
+        for i in range(self.N - 1):
+            self.events_queue.put((expovariate(1/16), order[i]))
+
+    def initial_infection(self, first_infect):
+        if first_infect is None:
+            first_infect = randint(0, self.N_common-1)
+        self.sim_network.nodes[first_infect].score = 1
+        self.sim_network.nodes[first_infect].reshare_rate = 1
+        self.sim_network.nodes[first_infect].recover_rate = 0
+        return first_infect
+
+    def simulate(self, max_time, recovered_debunking=False, SIR=False, first_infect=None):
+        self.events_queue = PriorityQueue()
+        self.sim_network = copy.deepcopy(self.network)
+        first_infect = self.initial_infection(first_infect)
+        self.sim_network.infected_node = first_infect
+        self.first_population_queue(first_infect)
+
+        # Add simulation checkpoint
+        for i in range(0, max_time, 20):
+            self.events_queue.put((i, -1))
+
+        s = []
+        i = []
+        r = []
+
+        time = 0
+        while time < max_time:
+            t, node_id = self.events_queue.get()
+
+            time = t
+
+            if node_id == -1:  # checkpoint
+                S = 0
+                I = 0
+                R = 0
+                for k in self.sim_network.nodes.keys():
+                    if self.sim_network.nodes[k].type == NodeType.Common:
+                        if self.sim_network.nodes[k].score == 1:
+                            I += 1
+                        elif self.sim_network.nodes[k].score == 0:
+                            S += 1
+                        else:
+                            R += 1
+                s.append(S)
+                i.append(I)
+                r.append(R)
+                continue
+
+            if not SIR:
+                self.sim_network.nodes[node_id].update()
+            else:
+                self.sim_network.nodes[node_id].update_sir()
+
+            score = self.sim_network.nodes[node_id].score
+
+            if score == 1 or (score == -1 and recovered_debunking):
+                reshare_rate = self.sim_network.nodes[node_id].reshare_rate
+                for edge in self.sim_network.nodes[node_id].adj:
+                    p = uniform(0, 1)
+                    dest = edge.dest
+                    weight = edge.weight
+                    if p < reshare_rate and dest != first_infect:
+                        self.propagate(dest, score, weight, SIR=SIR)
+
+            # se un nodo è un bot, allora si collega più spesso
+            if self.sim_network.nodes[node_id].type == NodeType.Bot:
+                self.events_queue.put((time + expovariate(1/4), node_id))
+            else:
+                self.events_queue.put((time + expovariate(1/16), node_id))
+
+        return s, i, r
+
+    def propagate(self, dest, score, weight, SIR):
+        if SIR:
+            self.sim_network.nodes[dest].message_queue.append(score)
+        else:
+            if score > 0:
+                En = self.engagement_news
+            else:
+                En = self.engagement_news * 0.5
+            message = En * score * weight
+            if message < - 1:
+                message = -1
+            elif message > 1:
+                message = 1
+            self.sim_network.nodes[dest].message_queue.append(message)
+
+def draw_simulation_network_roles(network):
+    G = nx.DiGraph()
+
+    pos = {}
+    color_map = []
+    for n, node in network.nodes.items():
+        G.add_node(n)
+        pos[n] = [node.position_x, node.position_y]
+
+        if node.type == NodeType.Common:
+            color_map.append("skyblue")
+        elif node.type == NodeType.Influencer:
+            color_map.append("gold")
+        elif node.type == NodeType.Bot:
+            color_map.append("red")
+
+    edge_labels = []
+    for a, node in network.nodes.items():
+        for b in node.adj:
+            G.add_edge(a, b.dest)
+            edge_labels.append(b)
+
+    f = plt.figure()
+    nx.draw(G, ax=f.add_subplot(111), pos=pos, with_labels=True, font_size=4, node_size=60, node_color=color_map, edge_color="grey", width=0.5, arrowsize=5)
+    f.savefig("graph.pdf")
+
+def draw_degree_distribution(network):
+    import collections
+    degree_sequence = []
+    for n, node in network.nodes.items():
+        degree_sequence.append(len(node.adj))
+
+    degreeCount = collections.Counter(degree_sequence)
+    deg, cnt = zip(*degreeCount.items())
+    fig, ax = plt.subplots()
+    plt.bar(deg, cnt, width=0.80, color='b')
+
+    plt.title("Degree Histogram")
+    plt.ylabel("Count")
+    plt.xlabel("Degree")
+    ax.set_xticks([d + 0.4 for d in deg])
+    ax.set_xticklabels(deg)
+    fig.savefig("degree.pdf")
+
+
+if __name__ == "__main__":
+    n_common = 200
+    n_influencer = 3
+    n_bots = 3
+    n_interests = 5
+    engagement_news = 1.0
+    vulnerability_avg = 0.5
+    vulnerability_var = 0.2
+    reshare_avg = 0.5
+    reshare_var = 0.2
+    recover_avg = 0.2
+    recover_var = 0.2
+    interests_avg = 0
+    interests_var = 0.4
+    random_const = 0.1
+    random_phy_const = 0.1
+    SIR = True
+    weighted = False
+    recovered_debunking = True
+    max_time = 1500
+
+    simulator = Simulator(N_common=n_common, N_influencers=n_influencer, N_interests=n_interests,
+                                    N_bots=n_bots, engagement_news=engagement_news,
+                                    random_const=random_const, random_phy_const=random_phy_const,
+                                    recover_avg=recover_avg, recover_var=recover_var,
+                                    vuln_avg=vulnerability_avg, vuln_var=vulnerability_var,
+                                    reshare_avg=reshare_avg, reshare_var=reshare_var,
+                                    int_avg=interests_avg, int_var=interests_var, weighted=weighted)
+
+
+    
+    S = []
+    I = []
+    R = []
+
+    N = 100
+    for c in range(N):
+        if c % 20 == 0:
+            print(c)
+        s, i, r = simulator.simulate(max_time, recovered_debunking, SIR, first_infect=None)
+        for k in range(len(s)):
+            if len(S) < len(s):
+                S.append(0)
+                I.append(0)
+                R.append(0)
+            S[k] += s[k]
+            I[k] += i[k]
+            R[k] += r[k]
+
+    for v in range(len(S)):
+        S[v] /= N
+        I[v] /= N
+        R[v] /= N
+
+    m = max(I)
+    xm = np.argmax(I)
+    M = []
+    for i in range(len(S)):
+        M.append(m)
+
+    plt.plot(S, label="S")
+    plt.plot(I, label="I")
+    plt.plot(R, label="R")
+    plt.plot(M, color="r", label="Max I")
+    plt.axvline(x=xm, color='r', linestyle='dashed')
+    plt.legend()
+    plt.savefig('common.pdf')
+    plt.clf()
+
+
+    simulator.add_influencers(n_influencer)
+
+    S = []
+    I = []
+    R = []
+    for c in range(N):
+        if c % 20 == 0:
+            print(c)
+        s, i, r = simulator.simulate(max_time, recovered_debunking, SIR, first_infect=None)
+        for k in range(len(s)):
+            if len(S) < len(s):
+                S.append(0)
+                I.append(0)
+                R.append(0)
+            S[k] += s[k]
+            I[k] += i[k]
+            R[k] += r[k]
+
+    for v in range(len(S)):
+        S[v] /= N
+        I[v] /= N
+        R[v] /= N
+
+    m = max(I)
+    M = []
+    xm = np.argmax(I)
+    for i in range(len(S)):
+        M.append(m)
+
+    plt.plot(S, label="S")
+    plt.plot(I, label="I")
+    plt.plot(R, label="R")
+    plt.plot(M, color="r", label="Max I")
+    plt.axvline(x=xm, color='r', linestyle='dashed')
+    plt.legend()
+    plt.savefig('influencers.pdf')
+    plt.clf()
+
+
+    simulator.add_bots(n_bots)
+    S = []
+    I = []
+    R = []
+    for c in range(N):
+        if c % 20 == 0:
+            print(c)
+        s, i, r = simulator.simulate(max_time, recovered_debunking, SIR, first_infect=None)
+        for k in range(len(s)):
+            if len(S) < len(s):
+                S.append(0)
+                I.append(0)
+                R.append(0)
+            S[k] += s[k]
+            I[k] += i[k]
+            R[k] += r[k]
+
+    for v in range(len(S)):
+        S[v] /= N
+        I[v] /= N
+        R[v] /= N
+
+    m = max(I)
+    xm = np.argmax(I)
+    M = []
+    for i in range(len(S)):
+        M.append(m)
+
+    plt.plot(S, label="S")
+    plt.plot(I, label="I")
+    plt.plot(R, label="R")
+    plt.plot(M, color="r", label="Max I")
+    plt.axvline(x=xm, color='r', linestyle='dashed')
+    plt.legend()
+    plt.savefig('bots.pdf')
+    plt.clf()
+
+    
+    draw_simulation_network_roles(simulator.network)
+    draw_degree_distribution(simulator.network)
